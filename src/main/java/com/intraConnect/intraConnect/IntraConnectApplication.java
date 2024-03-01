af7fae11c652862828743636da73d5c7ed74e1c5
EDCarlitosxD <juanuchdzib@gmail.com>
2024-03-01 12:06:18 -0500

    Empezamos el proyecto

---
+++ src/main/java/com/intraConnect/intraConnect/IntraConnectApplication.java
@@ -0,0 +1,13 @@
+package com.intraConnect.intraConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntraConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntraConnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/intraConnect/intraConnect/IntraConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intraConnect.intraConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntraConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
