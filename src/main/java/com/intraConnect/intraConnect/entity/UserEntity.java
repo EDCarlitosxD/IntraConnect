03f41b8d258a6f1034899fdc5a7ed5fe905ba48b
EDCarlitosxD <juanuchdzib@gmail.com>
2024-03-04 07:09:44 -0500

    UserDto, UserEntity UserDetailServiceImp lombok @Data and implements UserDetails

--- src/main/java/com/intraConnect/intraConnect/entity/UserEntity.java
+++ src/main/java/com/intraConnect/intraConnect/entity/UserEntity.java
@@ -1,10 +1,20 @@
 package com.intraConnect.intraConnect.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Entity
 @Table(name = "users")
-public class UserEntity {
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserEntity  implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,7 +31,43 @@ public class UserEntity {
     //private Departamento departamento;
 
     @Column(unique = true)
-    private String correo;
+    private String email;
+
+    private boolean enable = true;
 
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enable;
+    }
 }
---
+++ src/main/java/com/intraConnect/intraConnect/entity/dto/UserDto.java
@@ -0,0 +1,18 @@
+package com.intraConnect.intraConnect.entity.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+    private Long id;
+    private  String username;
+    private  String password;
+    private  String name;
+    //ROL
+    //private Departamento
+    private String email;
+}
